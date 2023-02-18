bd2bb665fd4a4a7b83d8d59b12844dc846d909c2
preethimulupoju <117334143+preethimulupoju@users.noreply.github.com>
2023-02-18 23:36:19 +0530

    5. Create a 10x10 array with random values and find the minimum and maximum values.

---
+++ maxmin.py
@@ -0,0 +1,10 @@
+import numpy as np
+n_array = np.random.random((10,10))
+
+print("the original array",n_array)
+
+print("\n\n")
+print("the minimum value of n_array: \n",n_array.min())
+      
+print("\n\n")
+print("the maximun value of n_array: \n",n_array.max())
